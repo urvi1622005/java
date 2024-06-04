bd9fd6772cf2dd8c55e27e91521bc1e420386a7b
urvi1622005 <urvashidhawan1622005>
2024-06-04 09:32:54 +0530

    change

---
+++ fibo.java
@@ -0,0 +1,9 @@
+
+import java.util.Scanner;
+public class fibo {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in)
+        char ch = int.next().trim().charAT(0);
+        
+    }
+}
---
+++ kkcondition.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class kkcondition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int c = sc.nextInt();    
+        // int sum = a+b+c;
+        // System.out.println(sum);
+    
+}
+
+// int max=0;
+// if(a>b) {
+//     max=a;
+// } else {
+//     max=b;
+// }
+// // System.out.println(max);
+// if(c>max) {
+//     max= c;
+// }
+// System.out.println(max);
+//     }
+// int max= Math.max(c,Math.max(a,b));
+System.out.println(Math.max(34,57));
+}
+char ch = in.next().trim().charAt(0);
+String word = "hello";
+System.out.println(word.charAt(2));
+if(ch >= 'a' && ch <= 'z') {
+    System.out.println("lowercase");
+}else {
+    System.out.println("uppercase");
+}
+    
+}
+System.out.println(ch);
+
+if(salery >10000)   {
+    salery += 2000;
+} else if (salery >20000) {
+    salery += 3000;
+} else {
+    salery += 1000;
+}
+System.out.println(salery);
+
+int a=10;
+int b=20;
+if(a==10 && b==20) {
+    System.out.println("true");
+} else {
+    System.out.println("false"); 
+}
+
