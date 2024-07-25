02377550b29c2f72efea881e66a6c047e7b6eaa1
urvi1622005 <urvashidhawan1622005@gmail.com>
2024-07-26 02:15:01 +0530

    .

---
+++ B.java
@@ -0,0 +1,11 @@
+public class B {
+      void abc(){
+        System.out.println("Hello");
+
+        
+    }
+    public static void main(String[] args) {
+        abc();
+    }
+    
+}
---
+++ practical1.java
@@ -0,0 +1,6 @@
+public class practical1 {
+    public static void main(String[] args) {
+        int n;
+    }
+    
+}
